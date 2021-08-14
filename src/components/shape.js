72e5eb2c7f6422f58404f724356c7d36403f42cb
Ashish Gupta <87773898+ashish030392@users.noreply.github.com>
2021-08-14 19:59:45 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
+// import using require
+var msg = require('./shape.js');
 // declare class
 
+
 // export class using module.exports
+
+module.exports= class Circle extends msg{
+ constructor(...args){
+  super(...args); 
+ }
+  calculateArea(){
+    console.log("2*pi*r"); 
+   return (2*pi*r);
+  }
+}
